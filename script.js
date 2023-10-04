ce56c7641733996988926f23bf892cc61c86ea70
DILIP CHOUDHARY <110682665+Dilip19IT19@users.noreply.github.com>
2023-10-04 12:40:05 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,13 +2,48 @@
 let inputVal=document.querySelector(".text");
 let addBtn=document.querySelector(".add");
 
+gsap.from("header,.add-box,.tasks-box",{
+  opacity:0,
+  scale:0,
+  stagger:0.5,
+  y:-200
+})
+
 inputVal.addEventListener("keydown",function(e){
 
   if(e.key==='Enter')
   {
-    if(inputVal.value.length===0)
-    {
-      alert("Empty task can't be added");
+    if(inputVal.value.toString().trim().length===0)
+    {   
+      gsap.from(".warning",{
+        y:-200,
+        opacity:0,
+        scale:0,
+        duration:0.5,
+        
+      })
+
+      document.querySelector(".warning").style.display="block";     
+      document.querySelector("header").style.filter=`blur(3px)`;
+      document.querySelector(".add-box").style.filter=`blur(3px)`;
+      document.querySelector(".tasks-box").style.filter=`blur(3px)`;
+      document.querySelector(".warning button").addEventListener("click",function(){
+        gsap.to(".warning",{
+          y:200,
+          scale:0,
+          opacity:0,
+          duration:0.5,         
+          display:"none"
+        })
+         
+        document.querySelector("header").style.filter=`blur(0px)`;
+      document.querySelector(".add-box").style.filter=`blur(0px)`;
+      document.querySelector(".tasks-box").style.filter=`blur(0px)`;
+      //document.querySelector(".warning").style.display="none";
+
+      })
+
+
     }
     else
     {
@@ -34,9 +69,40 @@ inputVal.addEventListener("keydown",function(e){
 
 addBtn.addEventListener("click",()=>{
 
-  if(inputVal.value.length===0)
+  if(inputVal.value.toString().trim().length===0)
     {
-      alert("Empty task can't be added");
+
+      gsap.from(".warning",{
+        y:-200,
+        opacity:0,
+        scale:0,
+        duration:0.5,
+        
+      })
+
+      
+      document.querySelector(".warning").style.display="block";
+      document.querySelector("header").style.filter=`blur(3px)`;
+      document.querySelector(".add-box").style.filter=`blur(3px)`;
+      document.querySelector(".tasks-box").style.filter=`blur(3px)`;
+      document.querySelector(".warning button").addEventListener("click",function(){
+
+        gsap.to(".warning",{
+          y:200,
+          scale:0,
+          opacity:0,
+          duration:0.5,    
+          display:"none"  
+          
+        })
+       
+        document.querySelector("header").style.filter=`blur(0px)`;
+      document.querySelector(".add-box").style.filter=`blur(0px)`;
+      document.querySelector(".tasks-box").style.filter=`blur(0px)`;
+      //document.querySelector(".warning").style.display="none";
+
+      })
+
     }
     else
     {
@@ -91,11 +157,18 @@ function showTasks(){
     </div>
     
     `
+   
+  
 
   })
 
   taskBox.innerHTML=tasks;
 
+  gsap.from(taskBox,{
+    opacity:0,
+    scale:0,
+    duration:0.5
+  })
 }
 
 function deleteTask(idx)
